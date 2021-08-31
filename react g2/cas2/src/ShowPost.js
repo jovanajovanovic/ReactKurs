9347a60b4010343a28dca175eab807abd6fce11d
Jovana Jovanovic <jovanaj33@gmail.com>
2021-08-31 19:24:14 +0200

    g2 - 31.08.2021.

--- react g2/cas2/src/ShowPost.js
+++ react g2/cas2/src/ShowPost.js
@@ -3,7 +3,19 @@ import './ShowPost.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const showInfo = (post) => {
+// ovde cu cuvati poslednji element na kojim sam kliknula 
+let selectElement = undefined; 
+
+const showInfo = (post, event) => {
+    // element na koji sam kliknula 
+    let click_element = event.target;
+    if(selectElement !== undefined){
+        selectElement.classList.toggle('click-element');
+    } 
+    // toggle - prima ime klase i gleda ako postoji klasa u listi klasa onda ce se izbrisati a ako ne postoji dodace je u listu 
+    click_element.classList.toggle('click-element'); 
+    selectElement = click_element;
+
     // prikazemo jedan post 
     let element =  (<div>
             <p>TITLE : {post.title}</p>
@@ -19,9 +31,9 @@ const showInfo = (post) => {
 // <ShowPost post={p}/>
 export function ShowPost (props) {
     let post = props.post;  // ovo mi je post koji trebam da prikazem 
-
+// e -> objekat event i mogu da pristupim njegovim atributima
     let jsx_element = (
-        <div className='post_container' onClick={() => showInfo(post)}>
+        <div className='post_container' onClick={(e) => showInfo(post, e)}>
             <p>TITLE : {post.title}</p>
             <p>ID : {post.id} </p> 
             <p>USER : {post.userId}</p>
